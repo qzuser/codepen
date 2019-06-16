64fee4b8a76ceaf6dab39917a7fc432f8ad7659c
qzuser <1522502267@qq.com>
2019-06-16 10:30:45 +0800

    优化一点点

---
+++ 图片轮播/NBA.js
@@ -0,0 +1,6 @@
+function slide(){
+  var pics = document.querySelectorAll(".slideshow img");
+  var pages = document.querySelectorAll(".page span");
+  var swap = document.querySelectorAll(".swap span");
+  
+}
--- 图片轮播/index.js
+++ 图片轮播/index.js
@@ -1,14 +1,13 @@
-//---------主角：轮播图函数-------------
 function slideshow() {
   var slideshow = document.getElementById("slideshow"),
-    imgs = slideshow.getElementsByTagName("img"), //得到图片们
-    pages = slideshow.getElementsByTagName("span"), //得到页码们
-    buttons = slideshow.getElementsByTagName("p"), //得到描述们
+    imgs = slideshow.getElementsByTagName("img"), 
+    pages = slideshow.getElementsByTagName("span"), 
+    buttons = slideshow.getElementsByTagName("p"),
     left_btn = buttons[0],
     right_btn = buttons[1],
     current = 0; //current为当前活跃的图片编号
 
-  function slideOff() {
+  function slideOff() {                   /*************要善于多用函数*******************/
     imgs[current].className = ""; //图片淡出
     pages[current].className = "";
   }
@@ -18,44 +17,39 @@ function slideshow() {
   }
 
   function changeSlide() {
-    //切换图片的函数
     slideOff();
-    current++; //自增1
+    current++;
     if (current >= 3) current = 0;
     slideOn();
   }
-
-  //每2s调用changeSlide函数进行图片轮播
   var slideon = setInterval(changeSlide, 2000);
 
   slideshow.onmouseover = function() {
-    clearInterval(slideon); //当鼠标移入时清除轮播事件
+    clearInterval(slideon); 
   };
   slideshow.onmouseout = function() {
-    slideon = setInterval(changeSlide, 2000); //当鼠标移出时重新开始轮播事件
+    slideon = setInterval(changeSlide, 2000);
   };
 
   for (var i = 0; i < pages.length; i++) {
-    //定义鼠标移入和移出页码事件
     pages[i].onmouseover = function() {
-      slideOff(); //图片淡出
+      slideOff(); 
       current = this.getAttribute("name"); //得到鼠标停留的页码对应的current
-      slideOn(); //图片淡出
+      slideOn();
     };
   }
 
   left_btn.onclick = function() {
-    slideOff(); //图片淡出
+    slideOff();
     current--;
     if (current < 0) current = 2;
-    slideOn(); //图片淡出
+    slideOn();
   };
   right_btn.onclick = function() {
-    slideOff(); //图片淡出
+    slideOff();
     current++;
     if (current > 2) current = 0;
-    slideOn(); //图片淡出
+    slideOn();
   };
 }
-
 slideshow();
