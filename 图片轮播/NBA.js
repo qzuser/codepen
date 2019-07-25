93f92044dc0e30b9a331f7628f8315e394f53b75
qzuser <1522502267@qq.com>
2019-07-25 10:09:47 +0800

    NBA轮播图

--- 图片轮播/NBA.js
+++ 图片轮播/NBA.js
@@ -1,6 +1,57 @@
-function slide(){
-  var pics = document.querySelectorAll(".slideshow img");
-  var pages = document.querySelectorAll(".page span");
-  var swap = document.querySelectorAll(".swap span");
-  
+var pics = document.querySelectorAll(".wrap img");
+var pages = document.querySelectorAll(".page span");
+var swap = document.querySelectorAll("p");
+var current = 0, timer;
+function slideout(){
+  pics[current].className = "";
+  pages[current].className = "";
 }
+function slidein(){
+  pics[current].className = 'active';
+  pages[current].className = 'active';
+}
+function change(){
+  timer = setInterval(() => {
+    slideout();
+    current++
+    if(current == 4) current = 0;
+    slidein();
+  }, 2000);
+}
+change();
+
+pics.forEach( item => {
+  item.addEventListener('mouseover', ()=>{
+    clearInterval(timer);
+  })
+  item.addEventListener('mouseout', () => {
+    change();
+  })
+})
+
+pages.forEach( item => {
+  item.addEventListener("mouseover", function(){
+    clearInterval(timer);
+    slideout();
+    current = this.dataset.page;
+    slidein();
+  });
+  item.addEventListener("mouseout", () => {
+    change();
+  });
+})
+
+swap[0].addEventListener("click", function() {
+  slideout();
+  current--;
+  if(current == -1) current = 3;
+  slidein();
+});
+
+swap[1].addEventListener('click', function(){
+  slideout();
+  current++;
+  if(current == 4) current = 0;
+  slidein();
+})
+
